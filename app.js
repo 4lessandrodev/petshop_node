90f1ded67783e8939d4174145c26034e03d9fd5e
Alessandro Menezes <alessandroadm@live.com>
2020-02-17 15:55:30 -0300

    Colocando resposta em utf8

--- app.js
+++ app.js
@@ -6,16 +6,26 @@ const callog = () => {
 };
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-type": "text/plain" });
-  //req => request
-  //res => response
+  res.writeHead(200, { "Content-type": "text/plain; charset=utf-8" });
+
+  //req => request, res => response
   switch (req.url) {
     case '/pets':
-      res.end('Aqui lista os pets de nossa aplicação');
+      res.write('Aqui lista os pets de nossa aplicação');
+      break;
+    case '/pets/add':
+      res.write('Adicionar um pet');
+      break;
+    case '/pets/buscar':
+      res.write('Buscar pet');
       break;
     default:
+      res.write('404 - Rota não encontrada');
       break;
   }
+
+  //Encerrar a request 
+  res.end();
 
   res.end('Olá mundo');
 }).listen(port, hostname, callog());
